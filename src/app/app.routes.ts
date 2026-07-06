6b10beba5080d3422ad97556ad63fd21f25c12df
José Romualdo <joseromualdo@outlook.com>
2026-07-06 11:05:10 -0300

    Vídeo 12: rotas no Angular (Home e Sobre)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent}, // Rota padrão
+    {path: 'sobre', component: SobreComponent}, // Rota sobre
+    {path: '**', redirectTo: ''} // Rota coringa
+];
